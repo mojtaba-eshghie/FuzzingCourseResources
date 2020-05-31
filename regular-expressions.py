5d326001447fa52884754e265d044996e1887fc7
vm1 <mojtaba.eshghi@ut.ac.ir>
2020-05-31 01:04:04 -0700

    adding the grammar mutations from grammars.ipynb

--- regular-expressions.py
+++ regular-expressions.py
@@ -3,6 +3,13 @@
 import re, random
 
 
+# to use the railroad diagram variation:
+from RailRoadDiagrams import NonTerminal, Terminal, Choice, HorizontalChoice, Sequence, Diagram, show_diagram
+from IPython.display import SVG, display
+from itertools import zip_longest
+import datetime
+
+
 
 grammar = {
     '<start>': ['<phone-number>'],
@@ -76,3 +83,69 @@ print(simple_grammar_fuzzer(grammar, START_SYMBOL, max_nonterminals=10, max_expa
 
 for i in range(10):
     print(simple_grammar_fuzzer(grammar=grammar, max_nonterminals=5))
+
+
+def syntax_diagram_symbol(symbol):
+    if is_nonterminal(symbol):
+        return NonTerminal(symbol[1:-1])
+    else:
+        return Terminal(symbol)
+
+def syntax_diagram_expr(expansion):
+    # In later chapters, we allow expansions to be tuples,
+    # with the expansion being the first element
+    if isinstance(expansion, tuple):
+        expansion = expansion[0]
+
+    symbols = [sym for sym in re.split(RE_NONTERMINAL, expansion) if sym != ""]
+    
+    if len(symbols) == 0:
+        symbols = [""]  # special case: empty expansion
+
+    return Sequence(*[syntax_diagram_symbol(sym) for sym in symbols])
+ 
+
+def syntax_diagram_alt(alt):
+    max_len = 5
+    alt_len = len(alt)
+    if alt_len > max_len:
+        iter_len = alt_len // max_len
+        alts = list(zip_longest(*[alt[i::iter_len] for i in range(iter_len)]))
+        exprs = [[syntax_diagram_expr(expr) for expr in alt
+                  if expr is not None] for alt in alts]
+        choices = [Choice(len(expr) // 2, *expr) for expr in exprs]
+        return HorizontalChoice(*choices)
+    else:
+        return Choice(alt_len // 2, *[syntax_diagram_expr(expr) for expr in alt])
+
+
+def syntax_diagram(grammar):
+    from IPython.display import SVG, display
+
+    for key in grammar:
+        print("%s" % key[1:-1])
+        #display(SVG(show_diagram(syntax_diagram_alt(grammar[key]))))
+
+        svg_display_obj = SVG(show_diagram(syntax_diagram_alt(grammar[key])))
+        
+        
+
+        now = datetime.datetime.now()
+        filename = now.strftime("%Y-%m-%d-%H-%M-%S" + key + ".svg")
+        with open('svg/' + filename, 'w') as svg_file:
+            svg_file.write(svg_display_obj.data)
+
+#svg_display_obj = SVG(show_diagram(syntax_diagram_symbol('<term>')))
+#svg_display_obj = SVG(show_diagram(syntax_diagram_expr(grammar['<start>'][0])))
+'''
+svg_display_obj = SVG(show_diagram(syntax_diagram_alt(grammar['<digit>'])))
+
+now = datetime.datetime.now()
+filename = now.strftime("%Y-%m-%d-%H-%M-%S.svg")
+with open('svg/' + filename, 'w') as svg_file:
+    svg_file.write(svg_display_obj.data)
+
+'''
+
+
+syntax_diagram(grammar)
